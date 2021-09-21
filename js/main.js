5c45449dad00712f5df3357276f965a53f902f30
VladShvec <vladshvec7@gmail.com>
2021-09-21 11:16:57 +0400

    firstproject

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.burger__menu').click(function(event){
+        $('.burger__menu,.nav').toggleClass('active')
+    });
+     
+});
+$(document).ready(function(){
+    $('.slides_blocks').slick();
+});
